0b2ec6c5f8e0bc0b3a915a9fd491c4383e81cef5
Craig Cooper <craig@stravinsky.(none)>
2013-05-15 11:41:04 +1000

    Initial import.

---
+++ src/ClusterNetworkLayer.cc
@@ -0,0 +1,439 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "BaseNetwLayer.h"
+
+#include <cassert>
+
+#include "NetwControlInfo.h"
+#include "BaseMacLayer.h"
+#include "AddressingInterface.h"
+#include "SimpleAddress.h"
+#include "FindModule.h"
+#include "ClusterControlMessage_m.h"
+#include "ArpInterface.h"
+#include "NetwToMacControlInfo.h"
+#include "BaseMobility.h"
+#include "BaseConnectionManager.h"
+#include "ChannelAccess.h"
+
+#include "ClusterNetworkLayer.h"
+
+Define_Module(ClusterNetworkLayer);
+
+
+void ClusterNetworkLayer::initialize(int stage)
+{
+    BaseNetwLayer::initialize(stage);
+
+    if(stage == 1) {
+
+    	// set up the node.
+    	mID = getId();
+    	mWeight = calculateWeight();
+    	mMobility = FindModule<BaseMobility*>::findSubModule(findHost());
+    	mClusterHead = -1;
+    	mIsClusterHead = false;
+
+    	ChannelAccess *channelAccess = FindModule<ChannelAccess*>::findSubModule(findHost());
+    	mTransmitRangeSq = pow( channelAccess->getConnectionManager( channelAccess->getParentModule() )->getMaxInterferenceDistance(), 2 );
+
+    	// load configurations
+    	mInitialFreshness = par("initialFreshness").doubleValue();
+    	mFreshnessThreshold = par("freshnessThreshold").doubleValue();
+    	mAngleThreshold = par("angleThreshold").doubleValue();
+    	mHopCount = par("hopCount").longValue();
+    	mBeaconInterval = par("beaconInterval").doubleValue();
+
+    	// set up self-messages
+    	mSendHelloMessage = new cMessage();
+    	scheduleAt( simTime() + mBeaconInterval, mSendHelloMessage );
+    	mFirstInitMessage = new cMessage();
+    	scheduleAt( simTime(), mSendHelloMessage );
+
+    }
+
+}
+
+
+
+
+
+/** @brief Handle messages from upper layer */
+void ClusterNetworkLayer::handleUpperMsg(cMessage* msg) {
+
+	assert(dynamic_cast<cPacket*>(msg));
+    sendDown(encapsMsg(static_cast<cPacket*>(msg)));
+
+}
+
+
+
+/** @brief Handle messages from lower layer */
+void ClusterNetworkLayer::handleLowerMsg(cMessage* msg) {
+
+    ClusterControlMessage *m = static_cast<ClusterControlMessage *>(msg);
+    coreEV << " handling packet from " << m->getSrcAddr() << std::endl;
+
+    switch( m->getKind() ) {
+
+		case HELLO_MESSAGE: receiveHelloMessage( m ); break;
+		case    CH_MESSAGE:    receiveChMessage( m ); break;
+		case  JOIN_MESSAGE:  receiveJoinMessage( m ); break;
+		case 		  DATA: sendUp( decapsMsg( m ) ); break;
+
+		default:
+		    coreEV << " received packet from " << m->getSrcAddr() << " of unknown type: " << m->getKind() << std::endl;
+		    delete msg;
+		    break;
+
+    };
+
+}
+
+/** @brief Handle self messages */
+void ClusterNetworkLayer::handleSelfMsg(cMessage* msg) {
+
+	if ( msg == mSendHelloMessage ) {
+
+		sendClusterMessage( HELLO_MESSAGE );
+    	scheduleAt( simTime() + mBeaconInterval, mSendHelloMessage );
+
+	} else if ( msg == mFirstInitMessage ) {
+
+		init();
+		delete mFirstInitMessage;
+
+	}
+
+}
+
+
+
+/** @brief decapsulate higher layer message from ClusterControlMessage */
+cMessage* ClusterNetworkLayer::decapsMsg( ClusterControlMessage *msg ) {
+
+    cMessage *m = msg->decapsulate();
+    setUpControlInfo(m, msg->getSrcAddr());
+
+    // delete the netw packet
+    delete msg;
+    return m;
+
+}
+
+
+/** @brief Encapsulate higher layer packet into a ClusterControlMessage */
+NetwPkt* ClusterNetworkLayer::encapsMsg( cPacket *appPkt ) {
+
+    LAddress::L2Type macAddr;
+    LAddress::L3Type netwAddr;
+
+    coreEV <<"in encaps...\n";
+
+    ClusterControlMessage *pkt = new ClusterControlMessage(appPkt->getName(), DATA);
+
+    pkt->setBitLength(headerLength);
+
+    cObject* cInfo = appPkt->removeControlInfo();
+
+    if(cInfo == NULL){
+		EV << "warning: Application layer did not specifiy a destination L3 address\n"
+		   << "\tusing broadcast address instead\n";
+		netwAddr = LAddress::L3BROADCAST;
+    } else {
+		coreEV <<"CInfo removed, netw addr="<< NetwControlInfo::getAddressFromControlInfo( cInfo ) << std::endl;
+			netwAddr = NetwControlInfo::getAddressFromControlInfo( cInfo );
+		delete cInfo;
+    }
+
+    pkt->setSrcAddr(myNetwAddr);
+    pkt->setDestAddr(netwAddr);
+    coreEV << " netw "<< myNetwAddr << " sending packet" <<std::endl;
+    if(LAddress::isL3Broadcast( netwAddr )) {
+        coreEV << "sendDown: nHop=L3BROADCAST -> message has to be broadcasted"
+           << " -> set destMac=L2BROADCAST\n";
+        macAddr = LAddress::L2BROADCAST;
+    }
+    else{
+        coreEV <<"sendDown: get the MAC address\n";
+        macAddr = arp->getMacAddr(netwAddr);
+    }
+
+    setDownControlInfo(pkt, macAddr);
+
+    //encapsulate the application packet
+    pkt->encapsulate(appPkt);
+    coreEV <<" pkt encapsulated\n";
+    return (NetwPkt*)pkt;
+
+}
+
+
+/** @brief Compute the CH weight for this node. */
+double ClusterNetworkLayer::calculateWeight() {
+
+	error( "ClusterNetworkLayer::calculateWeight MUST be overloaded to specify weight metric!" );
+	return 0;
+
+}
+
+
+
+
+/** @brief Initiate clustering. */
+void ClusterNetworkLayer::init() {
+
+	int nMax = chooseClusterHead();
+	if ( nMax == -1 ) {
+
+		// this node is the best CH around, so declare it
+		sendClusterMessage( CH_MESSAGE );
+		mIsClusterHead = true;
+		mClusterHead = mID;
+		mClusterMembers.clear();
+		mClusterMembers.insert( mID );
+
+	} else {
+
+		// we found a neighbour that's a better CH
+		sendClusterMessage( JOIN_MESSAGE, nMax );
+		mIsClusterHead = false;
+		mClusterHead = nMax;
+		mClusterMembers.clear();
+
+	}
+
+}
+
+
+
+/** @brief Select a CH from the neighbour table. */
+int ClusterNetworkLayer::chooseClusterHead() {
+
+	int nCurr = -1;
+	double wCurr = mWeight;
+
+	NeighbourSet::iterator it = mNeighbours.begin();
+	for ( ; it != mNeighbours.end(); it++ ) {
+
+		if ( it->second.mIsClusterHead && it->second.mWeight > wCurr ) {
+
+			nCurr = it->first;
+			wCurr = it->second.mWeight;
+
+		}
+
+	}
+
+	return nCurr;
+
+}
+
+
+
+/** @brief Handle a link failure. Link failure is detected when a CMs freshness reaches 0. */
+void ClusterNetworkLayer::linkFailure( unsigned int nodeId ) {
+
+	mNeighbours.erase( nodeId );
+	if ( mIsClusterHead )
+		mClusterMembers.erase( nodeId );
+	else if ( nodeId == mClusterHead )
+		init();
+
+}
+
+
+
+/** @brief Calculate the freshness of the given neighbour. */
+void ClusterNetworkLayer::calculateFreshness( unsigned int nodeId ) {
+
+	double freshness = mInitialFreshness;
+	Coord v =    mMobility->getCurrentSpeed() - mNeighbours[ nodeId ].mVelocity;
+	Coord p = mMobility->getCurrentPosition() - mNeighbours[ nodeId ].mPosition;
+
+	double a = v.squareLength(), b, c;
+	if ( a > 0 ) {
+
+		b = ( v.x * p.x + v.y * p.y ) / a;
+		c = ( p.squareLength() - mTransmitRangeSq ) / a;
+
+		double detSq = b*b - c;
+
+		if ( detSq > 0 ) {
+
+			double r1 = -b + sqrt( detSq );
+			double r2 = -b - sqrt( detSq );
+			if ( r1 > 0 && r2 > 0 )
+				freshness = std::min( 3*mInitialFreshness, std::min( r1, r2 ) );
+			else if ( r1 > 0 )
+				freshness = std::min( 3*mInitialFreshness, r1 );
+			else if ( r2 > 0 )
+				freshness = std::min( 3*mInitialFreshness, r2 );
+
+		}
+
+	}
+
+	mNeighbours[nodeId].mFreshness = freshness;
+
+}
+
+
+
+/** @brief Determine whether the given node is a suitable CH. */
+bool ClusterNetworkLayer::testClusterHeadChange( unsigned int nodeId ) {
+
+	bool t1 = mNeighbours[nodeId].mIsClusterHead;
+	bool t2 = mNeighbours[nodeId].mWeight > mNeighbours[mClusterHead].mWeight;
+	bool t3 = mNeighbours[nodeId].mFreshness >= mFreshnessThreshold;
+
+	Coord v1 = mMobility->getCurrentSpeed();
+	Coord v2 = mNeighbours[ nodeId ].mVelocity;
+	double angle = cos( ( v1.x*v2.x + v1.y*v2.y ) / ( v1.length() * v2.length() ) );
+
+	bool t4 = angle <= mAngleThreshold;
+
+	return t1 && t2 && t3 && t4;
+
+}
+
+
+/** @brief Handle a HELLO message. */
+void ClusterNetworkLayer::receiveHelloMessage( ClusterControlMessage *m ) {
+
+	updateNeighbour(m);
+
+	if ( testClusterHeadChange( m->getNodeId() ) ) {
+
+		sendClusterMessage( JOIN_MESSAGE, m->getNodeId() );
+		mIsClusterHead = false;
+		mClusterMembers.clear();
+
+	}
+
+	if ( m->getTtl() > 1 )
+		sendClusterMessage( HELLO_MESSAGE, -1, m->getTtl()-1 );
+
+	delete m;
+
+}
+
+
+
+/** @brief Handle a CH message. */
+void ClusterNetworkLayer::receiveChMessage( ClusterControlMessage *m ) {
+
+	updateNeighbour(m);
+
+	if ( testClusterHeadChange( m->getNodeId() ) ) {
+
+		sendClusterMessage( JOIN_MESSAGE, m->getNodeId() );
+		mIsClusterHead = false;
+		mClusterMembers.clear();
+
+	}
+
+	if ( m->getTtl() > 1 )
+		sendClusterMessage( CH_MESSAGE, -1, m->getTtl()-1 );
+
+	delete m;
+
+}
+
+
+
+/** @brief Handle a JOIN message. */
+void ClusterNetworkLayer::receiveJoinMessage( ClusterControlMessage *m ) {
+
+	updateNeighbour(m);
+
+	if ( mIsClusterHead ) {
+
+		if ( m->getTargetNodeId() == mID )
+			mClusterMembers.insert( m->getNodeId() );
+		else
+			mClusterMembers.erase( m->getNodeId() );
+
+	} else if ( mClusterHead == m->getNodeId() ) {
+
+		init();
+
+	}
+
+	if ( m->getTtl() > 1 )
+		sendClusterMessage( JOIN_MESSAGE, m->getTargetNodeId(), m->getTtl()-1 );
+
+	delete m;
+
+}
+
+/** @brief Update a neighbour's data. */
+void ClusterNetworkLayer::updateNeighbour( ClusterControlMessage *m ) {
+
+	// update the neighbour data
+	mNeighbours[m->getNodeId()].mWeight = m->getWeight();
+	mNeighbours[m->getNodeId()].mIsClusterHead = m->getIsClusterHead();
+	mNeighbours[m->getNodeId()].mPosition.x = m->getXPosition();
+	mNeighbours[m->getNodeId()].mPosition.y = m->getYPosition();
+	mNeighbours[m->getNodeId()].mVelocity.x = m->getXVelocity();
+	mNeighbours[m->getNodeId()].mVelocity.y = m->getYVelocity();
+	calculateFreshness( m->getNodeId() );
+
+}
+
+/** @brief Sends a given cluster message. */
+void ClusterNetworkLayer::sendClusterMessage( int kind, int dest, int nHops ) {
+
+    LAddress::L2Type macAddr;
+    LAddress::L3Type netwAddr;
+
+    coreEV <<"sending cluster control message...\n";
+
+	ClusterControlMessage *pkt = new ClusterControlMessage( "cluster-ctrl", kind );
+    pkt->setBitLength(headerLength);
+
+    // fill the cluster control fields
+    pkt->setNodeId( mID );
+    pkt->setWeight( mWeight );
+    pkt->setTtl( nHops );
+
+    Coord p = mMobility->getCurrentPosition();
+    pkt->setXPosition( p.x );
+    pkt->setYPosition( p.y );
+
+    p = mMobility->getCurrentSpeed();
+    pkt->setXVelocity( p.x );
+    pkt->setYVelocity( p.y );
+
+    pkt->setIsClusterHead( mIsClusterHead );
+    pkt->setTargetNodeId( dest );
+
+    netwAddr = LAddress::L3BROADCAST;
+
+    pkt->setSrcAddr(myNetwAddr);
+    pkt->setDestAddr(netwAddr);
+    coreEV << " netw "<< myNetwAddr << " sending packet" <<std::endl;
+	macAddr = LAddress::L2BROADCAST;
+
+    setDownControlInfo(pkt, macAddr);
+
+    sendDown( pkt );
+
+}
+
+
+
+
---
+++ src/ClusterNetworkLayer.h
@@ -0,0 +1,179 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __CLUSTERLIB_CLUSTERNETWORKLAYER_H_
+#define __CLUSTERLIB_CLUSTERNETWORKLAYER_H_
+
+#include <omnetpp.h>
+
+#include <MiXiMDefs.h>
+#include <BaseNetwLayer.h>
+
+#include <set>
+#include <map>
+
+/**
+ * This module implements the clustering mechanism for
+ * Modified Distributed Mobility-Aware Clustering (M-DMAC).
+ * There is a pure virtual function that must be implemented by
+ * inheriting cluster modules, which provides the weighting function
+ * for the particular cluster algorithm being tested.
+ */
+class ClusterNetworkLayer : public BaseNetwLayer
+{
+
+public:
+
+	/**
+	 * @brief Messages used by the clustering mechanism.
+	 */
+	enum ClusterMessageKinds {
+		HELLO_MESSAGE = LAST_BASE_NETW_MESSAGE_KIND+1,	/**< Periodic beacon used to update node weights. */
+		CH_MESSAGE,										/**< Announcement that a node has become a Cluster Head (CH). */
+		JOIN_MESSAGE,									/**< Announcement of a node's intention to join a CH. */
+		DATA,											/**< A datagram. */
+		LAST_CLUSTER_MESSAGE_KIND
+	};
+
+
+protected:
+
+	struct Neighbour {
+		double mWeight;						/**< Weight of this node. */
+		Coord mPosition;					/**< Position of the Neighbour. */
+		Coord mVelocity;					/**< Velocity of the Neighbour. */
+		bool mIsClusterHead;				/**< Is this node a CH? */
+		double mFreshness;					/**< How long this node will stay in range of this neighbour. */
+	};
+
+	typedef std::set<unsigned int> NodeIdSet;
+	typedef std::map<unsigned int,Neighbour> NeighbourSet;
+
+	unsigned int mID;						/**< Node's unique ID. */
+	double mWeight;							/**< Weight of this node. */
+	BaseMobility *mMobility;				/**< Pointer to the vehicle's mobility module (to get location and velocity). */
+	int mClusterHead;						/**< ID of the CH we're associated with (initialised to -1). */
+	bool mIsClusterHead;					/**< Is this node a CH? */
+	NodeIdSet mClusterMembers;				/**< Set of CMs associated with this node (if it is a CH) */
+	NeighbourSet mNeighbours;				/**< The set of neighbours near this node. */
+
+	double mTransmitRangeSq;				/**< Required for the freshness calculation. Obtained from the PhyLayer module. */
+
+    /**
+     * @name Messages
+     * @brief Messages this module sends itself.
+     *
+     * These are used for timed events.
+     *
+     **/
+    /*@{*/
+
+	cMessage *mFirstInitMessage;			/**< Run the cluster init function for the first time. */
+	cMessage *mSendHelloMessage;			/**< Send a HELLO message. */
+
+    /*@}*/
+
+
+    /**
+     * @name Parameters
+     * @brief Configurations for the module.
+     *
+     * These variables are set in the simulation configuration.
+     *
+     **/
+    /*@{*/
+
+	double mInitialFreshness;				/**< The initial node freshness. */
+	double mFreshnessThreshold;				/**< The minimum freshness for which a node is eligible to be a CH. */
+	double mAngleThreshold;					/**< The maximum angle between the directions of this node and another node for it to be considered for CH. */
+	unsigned int mHopCount;					/**< The number of hops for cluster control messages. */
+	double mBeaconInterval;					/**< The interval between each HELLO message. */
+
+    /*@}*/
+
+
+public:
+    //Module_Class_Members(BaseNetwLayer,BaseLayer,0);
+	ClusterNetworkLayer() : BaseNetwLayer() {}
+
+
+    /** @brief Initialization of the module and some variables*/
+    virtual void initialize(int);
+
+protected:
+
+    /** @brief Handle messages from upper layer */
+    virtual void handleUpperMsg(cMessage* msg);
+
+    /** @brief Handle messages from lower layer */
+    virtual void handleLowerMsg(cMessage* msg);
+
+    /** @brief Handle self messages */
+    virtual void handleSelfMsg(cMessage* msg);
+
+    /** @brief decapsulate higher layer message from ClusterControlMessage */
+    virtual cMessage* decapsMsg(ClusterControlMessage*);
+
+    /** @brief Encapsulate higher layer packet into a ClusterControlMessage */
+    virtual NetwPkt* encapsMsg(cPacket*);
+
+    /** @brief Compute the CH weight for this node. */
+    virtual double calculateWeight();
+
+    /**
+     * @name Cluster Methods
+     * @brief Methods handling the formation and maintenance of clusters.
+     *
+     * These methods are implementations of the M-DMAC functions as specified
+     * in the paper "Modified DMAC Clustering Algorithm for VANETs" by G. Wolny
+     *
+     **/
+    /*@{*/
+
+    /** @brief Initiate clustering. */
+    void init();
+
+    /** @brief Select a CH from the neighbour table. */
+    int chooseClusterHead();
+
+    /** @brief Handle a link failure. Link failure is detected when a CMs freshness reaches 0. */
+    void linkFailure( unsigned int );
+
+    /** @brief Calculate the freshness of the given neighbour. */
+    void calculateFreshness( unsigned int );
+
+    /** @brief Determine whether the given node is a suitable CH. */
+    bool testClusterHeadChange( unsigned int );
+
+    /** @brief Handle a HELLO message. */
+    void receiveHelloMessage( ClusterControlMessage* );
+
+    /** @brief Handle a CH message. */
+    void receiveChMessage( ClusterControlMessage* );
+
+    /** @brief Handle a JOIN message. */
+    void receiveJoinMessage( ClusterControlMessage* );
+
+    /** @brief Update a neighbour's data. */
+    void updateNeighbour( ClusterControlMessage* );
+
+    /** @brief Sends a given cluster message. */
+    void sendClusterMessage( int, int = -1, int = -1 );
+
+    /*@}*/
+
+};
+
+#endif
